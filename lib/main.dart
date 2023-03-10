02e04b7b0fbf81d1cc2c934ac520113f88bd8872
B.Karim <41644989+Karim-bz@users.noreply.github.com>
2023-03-10 11:44:25 +0100

    Screen title Animation - Tween Animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animation_workshop/screens/home.dart';
-import 'package:flutter_animation_workshop/screens/sandbox.dart'; 
+import 'package:flutter_animation_workshop/screens/home.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Ninja Trips',
-      home: SandBox(),
+      home: Home(),
     );
   }
 }
--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -7,9 +7,26 @@ class ScreenTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+    return TweenAnimationBuilder(
+      
+      tween: Tween<double>(begin: 0,end: 1),
+      duration: const Duration(milliseconds: 500), 
+      builder: (BuildContext context, double _val, Widget? child) {
+        // the title will animate from the top
+        return Opacity(
+          opacity: _val,
+          child: Padding(
+            padding: EdgeInsets.only(top: _val * 20),
+            child: child,
+          )
+        );
+      },
+      child: Text(
+        text,
+        style: const TextStyle(
+            fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      
     );
   }
 }
