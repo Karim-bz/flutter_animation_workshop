03d7a46d09ac3adc425cb68badf73c9f16f2a785
B.Karim <41644989+Karim-bz@users.noreply.github.com>
2023-03-10 11:51:13 +0100

    details page using Hero Animation

--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -8,25 +8,22 @@ class ScreenTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TweenAnimationBuilder(
-      
-      tween: Tween<double>(begin: 0,end: 1),
-      duration: const Duration(milliseconds: 500), 
+      tween: Tween<double>(begin: 0, end: 1),
+      duration: const Duration(milliseconds: 500),
       builder: (BuildContext context, double _val, Widget? child) {
         // the title will animate from the top
         return Opacity(
-          opacity: _val,
-          child: Padding(
-            padding: EdgeInsets.only(top: _val * 20),
-            child: child,
-          )
-        );
+            opacity: _val,
+            child: Padding(
+              padding: EdgeInsets.only(top: _val * 20),
+              child: child,
+            ));
       },
       child: Text(
         text,
         style: const TextStyle(
             fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
       ),
-      
     );
   }
 }
